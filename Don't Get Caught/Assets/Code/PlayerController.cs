082913ac3208e37f48b74b690a10d4a7e3e8bcb1
Harris <h.s.kim@wustl.edu>
2025-02-07 20:09:02 -0600

    created door prefab

---
+++ Don't Get Caught/Assets/Code/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Update()
+    {
+        Keyboard keyboardInput = Keyboard.current;
+        Mouse mouseInput = Mouse.current;
+        if(keyboardInput != null && mouseInput != null)
+        {
+            if(keyboardInput.eKey.wasPressedThisFrame)
+            {
+                Vector2 mousePosition = mouseInput.position.ReadValue();
+
+                Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+                RaycastHit hit;
+                if(Physics.Raycast(ray, out hit, 2f))
+                {
+                    print("Interacted with " + hit.transform.name + " from " + hit.distance + "m.");
+
+                    Door targetDoor = hit.transform.GetComponent<Door>();
+                    if(targetDoor)
+                    {
+                        targetDoor.Interact();
+                    }
+                }
+            }
+        }
+    }
+}
