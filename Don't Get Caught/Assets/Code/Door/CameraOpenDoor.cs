22032e34c3125a4af94a9bf322d3ded94e5f22e4
Mijung Jung <j.mijung@wustl.edu>
2025-02-22 01:05:23 -0600

    fixed tutorial and make game more complicated

--- Don't Get Caught/Assets/Code/Door/CameraOpenDoor.cs
+++ Don't Get Caught/Assets/Code/Door/CameraOpenDoor.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 public class CameraOpenDoor : MonoBehaviour
 {
 	public float DistanceOpen = 3;
-	public GameObject text;
 	private PlayerInventory playerInventory;
 	public DoorScript.Door door;
 
@@ -23,16 +22,10 @@ public class CameraOpenDoor : MonoBehaviour
 		if (Physics.Raycast(transform.position, transform.forward, out hit, DistanceOpen))
 
 		{
-			if (hit.transform.GetComponent<WrongDoor>())
-			{
-				text.SetActive(true);
-			}
-
 
 			if (hit.transform.GetComponent<DoorScript.Door>())
 
 			{
-				text.SetActive(true);
 				if (Input.GetKeyDown(KeyCode.E))
 				{
 					if (door != null && playerInventory != null && playerInventory.HasItem("Key"))
@@ -42,15 +35,9 @@ public class CameraOpenDoor : MonoBehaviour
 					}
 				}
 			}
-			else
-			{
-				text.SetActive(false);
-			}
+
 		}
-		else
-		{
-			text.SetActive(false);
-		}
+
 	}
 }
 
